255d29aa7b3e4fd37229cf4a4566f9fdd16086c3
linduxed <linduxed@gmail.com>
2013-04-08 21:21:46 +0200

    Rewritten to use Pascal's Triangle.

--- haskell/15.hs
+++ haskell/15.hs
@@ -5,32 +5,36 @@
  -
  - How many such routes are there through a 20×20 grid?
  -}
-data Direction = DRight | DDown deriving (Show, Eq)
-type Position  = (Integer, Integer)
-type Grid      = (Integer, Integer)
 
+{-
+ - Upon inspection, the amount of paths is directly proportional to the center
+ - values of the odd numbered rows in Pascal's Triangle:
+ -
+ -             1
+ -           1   1
+ -         1   2   1         <- 1x1 grid has 2 paths
+ -       1   3   3   1
+ -     1   4   6   4   1     <- 2x2 grid has 6 paths
+ -   1   5   10  10  5   1
+ - 1   6   15  20  15  6   1 <- 3x3 grid has 20 paths
+ -
+ - Calculating the triangle and using these values is far less expensive than
+ - traversing all the paths.
+ -
+ - Another way of seeing it is as the binomial "40 choose 20".
+ -}
 problem15 :: Int
-problem15 = length $ pickAllPaths (20, 20)
+problem15 = triangleValue $ gridSizeToTriangleRow 20
 
-pickAllPaths :: Grid -> [[Direction]]
-pickAllPaths gridSize = branchLoop (availableDirections start) [] start
+-- There should be an addition of one at the end here, but since list positions
+-- start at 0, this addition is omitted to compensate.
+gridSizeToTriangleRow :: Int -> Int
+gridSizeToTriangleRow size = size * 2
+
+triangleValue :: Int -> Int
+triangleValue rowNumber = pascal !! rowNumber !! (rowNumber `div` 2)
+
+pascal :: [[Int]]
+pascal = iterate f [1]
   where
-    start = (0, 0)
-
-    branchLoop :: [Direction] -> [Direction] -> Position -> [[Direction]]
-    branchLoop [] path _                   = [path]
-    branchLoop directions currPath currPos = concatMap branchHelper directions
-      where
-        branchHelper x = branchLoop (availableDirections (nextPos x)) (x:currPath) (nextPos x)
-        nextPos = oneStep currPos
-
-    availableDirections position
-        | position     == gridSize     = []       -- Reached bottom-right corner.
-        | fst position == fst gridSize = [DDown]  -- Reached right border.
-        | snd position == snd gridSize = [DRight] -- Reached bottom.
-        | otherwise                    = [DRight, DDown]
-
-oneStep :: Position -> Direction -> Position
-oneStep (x, y) nextPosirection
-    | nextPosirection == DRight = (x+1, y  )
-    | otherwise                 = (x  , y+1)
+    f xs = zipWith (+) (0:xs) (xs ++ [0])
