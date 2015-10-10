d2e3ace1eb0c69d039767362150a3a8d52c1dd0b
Mikael Muszynski <linduxed@gmail.com>
2015-10-10 16:52:59 +0200

    Tweak solution to #9 in Haskell
    * Split list comprehension across multiple lines.
    * Turn single element list into let-statement.

--- haskell/9.hs
+++ haskell/9.hs
@@ -9,4 +9,9 @@
  -}
 
 pyTripletProduct :: Integer
-pyTripletProduct = product $ head [ [a,b,c] | a <- [1..1000], b <- [1..1000], c <- [1000 - a - b], a^2 + b^2 == c^2 ]
+pyTripletProduct = product $ head [ [a,b,c]
+                                  | a <- [1..1000]
+                                  , b <- [1..1000]
+                                  , let c = 1000 - a - b
+                                  , a^2 + b^2 == c^2
+                                  ]
